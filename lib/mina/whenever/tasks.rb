b98ac727c047ca8891d76396384cd2ba84875737
Ian Dickson <ipd@dicksonlabs.com>
2017-07-31 15:47:31 -0700

    Added support for setting arbitrary key/value pairs to pass to Whenever.
    Set them in `deploy.rb`:
    
        set :whenver_sets, our_host: ENV['host']
    
    Use them in `schedule.rb`:
    
        if @our_host == 'starfleet.org'
            ...
        end

--- lib/mina/whenever/tasks.rb
+++ lib/mina/whenever/tasks.rb
@@ -1,11 +1,25 @@
 set :whenever_name, -> { "#{fetch(:domain)}_#{fetch(:rails_env)}" }
 
 namespace :whenever do
+
+  # ### whenever_sets
+  # Pass optional name=value pairs to the --set argument of whenever.
+  # Specify these in your deploy.rb as key/value pairs, for example:
+  #     set :whenever_sets, host: ENV['HOST']
+
+  sets = lambda { whenever_sets || {} }
+
+  set_string = lambda do
+    ["environment=#{fetch(:rails_env)}",
+     "path=#{fetch(:current_path)}",
+     sets.call.map { |k, v| "#{k}=#{v}" }].flatten.join('&')
+  end
+
   desc 'Clear crontab'
   task clear: :environment do
     comment "Clear contrab for #{fetch(:whenever_name)}"
     in_path fetch(:current_path) do
-      command "#{fetch(:bundle_bin)} exec whenever --clear-crontab #{fetch(:whenever_name)} --set 'environment=#{fetch(:rails_env)}&path=#{fetch(:current_path)}'"
+      command "#{fetch(:bundle_bin)} exec whenever --clear-crontab #{fetch(:whenever_name)} --set '#{set_string.call}'"
     end
   end
 
@@ -13,7 +27,7 @@ namespace :whenever do
   task update: :environment do
     comment "Update crontab for #{fetch(:whenever_name)}"
     in_path fetch(:current_path) do
-      command "#{fetch(:bundle_bin)} exec whenever --update-crontab #{fetch(:whenever_name)} --set 'environment=#{fetch(:rails_env)}&path=#{fetch(:current_path)}'"
+      command "#{fetch(:bundle_bin)} exec whenever --update-crontab #{fetch(:whenever_name)} --set '#{set_string.call}'"
     end
   end
 
@@ -21,7 +35,7 @@ namespace :whenever do
   task write: :environment do
     comment "Write crontab for #{fetch(:whenever_name)}"
     in_path fetch(:current_path) do
-      command "#{fetch(:bundle_bin)} exec whenever --write-crontab #{fetch(:whenever_name)} --set 'environment=#{fetch(:rails_env)}&path=#{fetch(:current_path)}'"
+      command "#{fetch(:bundle_bin)} exec whenever --write-crontab #{fetch(:whenever_name)} --set '#{set_string.call}'"
     end
   end
 end
